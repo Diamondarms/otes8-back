20fd7f14d98026d5b0006a7ddbf8b12f3ff847d5
guilherme.tonon <guilherme.tonon@martinellilabs.com.br>
2025-11-21 17:45:26 -0300

    att vercel

---
+++ api/index.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import createApp from '../src/app';
+
+let appPromise: Promise<any> | null = null;
+
+export default async function handler(req: Request, res: Response) {
+    if (!appPromise) {
+        appPromise = createApp();
+    }
+    
+    const app = await appPromise;
+    
+    app(req, res);
+}
--- src/db.ts
+++ src/db.ts
@@ -6,17 +6,22 @@ if (!connectionString) {
     throw new Error('A variável de ambiente DATABASE_URL não está configurada.');
 }
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const client = new Client({
     connectionString: connectionString,
-    ssl: false
+    ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 async function dbConnect() {
     try {
-        await client.connect();
-        console.log("Conexão com o banco de dados foi estabelecida com sucesso!");
+        // @ts-ignore
+        if (!client._connected) { 
+            await client.connect();
+            console.log("Conexão com o banco de dados foi estabelecida com sucesso!");
+        }
     } catch (err) {
-        console.error("Erro na conexão com o Supabase:", err);
+        console.error("Erro na conexão com o Banco:", err);
     }
 }
 
