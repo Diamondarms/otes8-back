bb4edef7d8d810d17217a9bc2d084b2c14b92283
guilherme.tonon <guilherme.tonon@martinellilabs.com.br>
2025-11-21 18:22:11 -0300

    fix: cors

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,11 @@ import createApp from '../src/app';
 let appPromise: Promise<any> | null = null;
 
 export default async function handler(req: Request, res: Response) {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     if (!appPromise) {
         appPromise = createApp();
     }
